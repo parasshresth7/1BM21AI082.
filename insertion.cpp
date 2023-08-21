42f018abc282919f8b71ca1ec7bad9ae6c58d5af
Paras shresth <138446761+parasshresth7@users.noreply.github.com>
2023-08-21 14:48:21 +0530

    Add files via upload

---
+++ insertion.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+
+void insertionSort(std::vector<int>& arr) {
+    int size = arr.size();
+
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+
+void printArray(const std::vector<int>& arr) {
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main() {
+    std::vector<int> arr = {9, 3, 17, 1, 25, 6, 11};
+
+    std::cout << "Original array: ";
+    printArray(arr);
+
+    insertionSort(arr);
+
+    std::cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
---
+++ mergesort.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+
+void merge(std::vector<int>& arr, int left, int mid, int right) {
+    int i = left;
+    int j = mid + 1;
+    int k = 0;
+    std::vector<int> temp(right - left + 1);
+
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+
+
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+
+
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+
+    for (i = left, k = 0; i <= right; i++, k++) {
+        arr[i] = temp[k];
+    }
+}
+
+
+void mergeSort(std::vector<int>& arr, int left, int right) {
+    if (left >= right) {
+
+        return;
+    }
+
+    int mid = left + (right - left) / 2;
+
+
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+}
+
+
+void printArray(const std::vector<int>& arr) {
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<int> arr = {9, 3, 17, 1, 25, 6, 11};
+    int size = arr.size();
+
+    std::cout << "Original array: ";
+    printArray(arr);
+
+    mergeSort(arr, 0, size - 1);
+
+    std::cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
